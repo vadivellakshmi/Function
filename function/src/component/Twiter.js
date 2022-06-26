91e4587078e2f2be24a7c73d485138475e74548f
LAKSHMI PRIYA <101013472+vadivellakshmi@users.noreply.github.com>
2022-06-26 18:11:29 +0530

    Update Twiter.js

--- function/src/component/Twiter.js
+++ function/src/component/Twiter.js
@@ -4,7 +4,7 @@ const Twiter = () =>{
     const  [list, setList] = useState([]);
     const [search, setSearch] = useState([]);
     useEffect(() =>{
-        axios({url:"http://fakeapi.jsonparseronline.com/posts",
+        axios({url:"https://jsonplaceholder.typicode.com/todos",
          method: "GET"}).then((res) => {
             console.log(res.data);
             setList(res.data)
@@ -17,23 +17,23 @@ const Twiter = () =>{
     return(
        
             <div className={"container justify-content-center align-items-center"}>
-               <h1 className="display-5 text-success me-3">Content Views</h1> <input type="number" placeholder="enter-like-count" onChange={(e) =>{setSearch(e.target.value)}}/>
+               <h1 className="display-5 text-success me-3">Content Views</h1> <input type="text" placeholder="enter-like-count" onChange={(e) =>{setSearch(e.target.value)}}/>
                  {list.filter(item =>{
                     if(search == ""){return item}
-                    else if(search == item.likes){return item}
+                    else if(search == item.completed){return item}
                  }).map((item) =>{
                      return(
                         
                 <div className="card w-auto">
               <div className="card-header text-primariy">
-              VIEWS-ID {item.userId}
+              VIEWS-ID {item.id}
                       </div>
                  <div className="card-body">
                <h5 className="card-title text-success"> {item.title}</h5>
                <p className="card-text"> {item.content}</p>
-               <a href="#" className="btn btn-primary me-3">LIKES <i class="fa-solid fa-heart"></i> <span className="badge bg-secondary">{item.likes}</span> </a> 
+               <a href="#" className="btn btn-primary me-3">LIKES <i class="fa-solid fa-heart"></i> <span className="badge bg-secondary">{item.completed}</span> </a> 
                <button type="button" className="btn btn-primary">
-                HITS  <i class="fa-solid fa-thumbs-up"></i> <span className="badge bg-secondary">{item.hits}</span>
+                HITS  <i class="fa-solid fa-thumbs-up"></i> <span className="badge bg-secondary">{item.userId}</span>
                   </button>
                <span className ="float-end"><i className="fa-brands fa-twitter"></i></span>
                </div>
